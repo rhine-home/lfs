b5ee981e08272c800c92da1ab1a750ef6fff859d
Mearaj Ahmad <mearajahmad@gmail.com>
2019-10-13 14:34:05 +0530

    Updated version checker

--- version_check.sh
+++ version_check.sh
@@ -37,7 +37,8 @@ tar --version | head -n1
 makeinfo --version | head -n1 # texinfo version
 xz --version | head -n1
 
-echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c if [ -x dummy ]
+echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
+if [ -x dummy ]
   then echo "g++ compilation OK";
-else echo "g++ compilation failed"; fi rm -f dummy.c dummy
-
+  else echo "g++ compilation failed"; fi
+rm -f dummy.c dummy
